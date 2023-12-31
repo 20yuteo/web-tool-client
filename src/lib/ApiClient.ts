b7841a94d79141c81ed343365b1b8503fec68b9f
20yuteo <20yuteo@gmail.com>
2023-12-31 13:12:10 +0900

    カスタムドメイン設定した

--- src/lib/ApiClient.ts
+++ src/lib/ApiClient.ts
@@ -10,8 +10,8 @@ type ResponseIF = {
 
 export async function apiClient<Response>(request: RequestIF): Promise<Response & ResponseIF> {
   try {
-    // const result = await fetch(`http://localhost:3000/dev${request.path}`, {
-    const result = await fetch(`https://ntsrfhf53c.execute-api.ap-northeast-1.amazonaws.com/dev${request.path}`, {
+    const result = await fetch(`https://wta.meet-app.link/${request.path}`, {
+    // const result = await fetch(`https://ntsrfhf53c.execute-api.ap-northeast-1.amazonaws.com/dev${request.path}`, {
       headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
