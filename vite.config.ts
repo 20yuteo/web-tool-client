aa96bd99dc45229f59c6dc1d6f9bb699e9bf6902
20yuteo <20yuteo@gmail.com>
2024-01-04 23:59:36 +0900

    ログインログアウトとりあえずできるようになった

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 8000,
-  }
+  },
+  resolve: {
+    alias: {
+      '@': '/src/*',
+      // '@lib/': '/src/lib/*',
+      // '@components/': '/src/components/*',
+    },
+  },
 })
