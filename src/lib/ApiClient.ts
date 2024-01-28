8db96b2756e8921e37c88935417909c6efbe8d59
20yuteo <20yuteo@gmail.com>
2024-01-28 22:34:10 +0900

    apiClient修正

--- src/lib/ApiClient.ts
+++ src/lib/ApiClient.ts
@@ -1,32 +1,35 @@
 type RequestIF = {
   method: "GET" | "POST" | "PUT" | "DELETE";
   path: string;
-  body?: Record<string, unknown>; 
-}
+  body?: Record<string, unknown>;
+  headers?: HeadersInit;
+};
 
 type ResponseIF = {
   ok: boolean;
-}
+};
 
 const apiURL = import.meta.env.VITE_API_URL || "http://localhost:3000/dev";
 
-export async function apiClient<Response>(request: RequestIF): Promise<Response & ResponseIF> {
+export async function apiClient<Response>(
+  request: RequestIF
+): Promise<Response & ResponseIF> {
   try {
     const result = await fetch(`${apiURL}${request.path}`, {
       headers: {
         "Content-Type": "application/json",
+        ...request.headers,
       },
       mode: "cors",
       method: request.method,
       body: JSON.stringify(request.body),
     });
-  
+
     const res: Response = await result.json();
     return {
       ok: result.ok,
-      res
+      res,
     } as unknown as Response & ResponseIF;
-
   } catch {
     throw new Error("API Error");
   }
